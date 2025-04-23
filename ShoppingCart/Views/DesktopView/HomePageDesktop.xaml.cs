ee6891266156a3c237fdf7ee7ddc75318614af37
Karthikasf4617 <karthika.sf4617@gmail.com>
2025-04-23 20:07:45 +0530

    Updated layout changes for Windows and Android, and added product details in the ViewModel.

--- ShoppingCart/Views/DesktopView/HomePageDesktop.xaml.cs
+++ ShoppingCart/Views/DesktopView/HomePageDesktop.xaml.cs
@@ -1,17 +1,222 @@
-﻿using Syncfusion.Maui.Rotator;
+using Syncfusion.Maui.Rotator;
 using System.Collections.ObjectModel;
+using static ShoppingCart.ShoppingCartViewModel;
 
 namespace ShoppingCart
 {
-    public partial class HomePageDesktop : ContentPage
+    public partial class HomePageDesktop : ContentView
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnOneCollection { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnTwoCollection { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnThreeCollection { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnFourCollection { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnFiveCollection { get; set; }
+
+
+        ShoppingCartViewModel shoppingCartViewModel = new ShoppingCartViewModel();
         public HomePageDesktop()
         {
             InitializeComponent();
-            ShoppingCartViewModel shoppingCartViewModel = new ShoppingCartViewModel();
-            rotatorView.ItemsSource = shoppingCartViewModel.DesktopRotatorItems;
-            catagoriesChip.ItemsSource = shoppingCartViewModel.Catagories;
+            var selectedCategory = "Men";
+            if (selectedCategory != null)
+            {
+
+                if (shoppingCartViewModel != null)
+                {
+                    shoppingCartViewModel.FilteredProducts = new ObservableCollection<Product>();
+                    var filteredProducts = shoppingCartViewModel.Products
+                        .Where(product => product.Category == selectedCategory);
+
+                    foreach (var product in filteredProducts)
+                    {
+                        shoppingCartViewModel.FilteredProducts.Add(product);
+                    }
+                }
+            }
+             BindingContext = shoppingCartViewModel;
+        }
+
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                UpdateColumn(width);
+            });
+        }
+
+        internal void UpdateColumn(double width)
+        {
+            if (width > 900)
+            {
+                this.ProductGrid.ColumnDefinitions.Clear();
+                this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                ArrangeControlInFiveColumn();
+            }
+            if (width > 700 && width <= 900)
+            {
+                if(this.ProductGrid!=null)
+                {
+                    this.ProductGrid.ColumnDefinitions.Clear();
+                    this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    ArrangeControlInThreeColumn();
+                }
+            }
+            else if(width < 700)
+            {
+                if (this.ProductGrid != null)
+                {
+                    this.ProductGrid.ColumnDefinitions.Clear();
+                    this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    this.ProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                    ArrangeControlInTwoColumn();
+                }
+            }
+        }
+
+        private void ArrangeControlInTwoColumn()
+        {
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            for (int i = 0; i < shoppingCartViewModel.FilteredProducts?.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ColumnOneCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else 
+                {
+                    ColumnTwoCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+            }
+            ColumnThreeBorder.IsVisible = false;
+            ColumnFourBorder.IsVisible = false;
+            ColumnFiveBorder.IsVisible = false;
+            BindableLayout.SetItemsSource(this.ColumnThreeBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnFourBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnFiveBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
+        }
+
+        private void ArrangeControlInThreeColumn()
+        {
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            ColumnThreeCollection = new List<Product>();
+            for (int i = 0; i < shoppingCartViewModel.FilteredProducts?.Count; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    ColumnOneCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else if (i % 3 == 1)
+                {
+                    ColumnTwoCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else
+                {
+                    ColumnThreeCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+            }
+            ColumnThreeBorder.IsVisible = true;
+            ColumnFourBorder.IsVisible = false;
+            ColumnFiveBorder.IsVisible = false;
+            BindableLayout.SetItemsSource(this.ColumnFourBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnFiveBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
+            BindableLayout.SetItemsSource(this.ColumnThreeBorder, ColumnThreeCollection);
+        }
+        private void ArrangeControlInFiveColumn()
+        {
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            ColumnThreeCollection = new List<Product>();
+            ColumnFourCollection = new List<Product>();
+            ColumnFiveCollection = new List<Product>();
+            for (int i = 0; i < shoppingCartViewModel.FilteredProducts?.Count; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    ColumnOneCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else if (i % 5 == 1)
+                {
+                    ColumnTwoCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else if (i % 5 == 2)
+                {
+                    ColumnThreeCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else if (i % 5 == 3)
+                {
+                    ColumnFourCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else
+                {
+                    ColumnFiveCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+            }
+            ColumnFourBorder.IsVisible = true;
+            ColumnFiveBorder.IsVisible = true;
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnThreeBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnFourBorder, null);
+            BindableLayout.SetItemsSource(this.ColumnFiveBorder, null);
+
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
+            BindableLayout.SetItemsSource(this.ColumnThreeBorder, ColumnThreeCollection);
+            BindableLayout.SetItemsSource(this.ColumnFourBorder, ColumnThreeCollection);
+            BindableLayout.SetItemsSource(this.ColumnFiveBorder, ColumnThreeCollection);
+
+        }
+        private void catagoriesChip_SelectionChanged(object sender, Syncfusion.Maui.Core.Chips.SelectionChangedEventArgs e)
+        {
+            var selectedCategory = e.AddedItem;
+            if (selectedCategory != null)
+            {
+                if (shoppingCartViewModel != null)
+                {
+                    shoppingCartViewModel.FilteredProducts?.Clear();
+
+                    var filteredProducts = shoppingCartViewModel.Products
+                        .Where(product => product.Category == selectedCategory);
+
+                    foreach (var product in filteredProducts)
+                    {
+                        shoppingCartViewModel.FilteredProducts?.Add(product);
+                    }
+                }
+            }
+            UpdateColumn(this.Width);
         }
     }
 }
--- ShoppingCart/Views/MobileView/MainPageMobile.xaml.cs
+++ ShoppingCart/Views/MobileView/MainPageMobile.xaml.cs
@@ -5,12 +5,26 @@ namespace ShoppingCart
 {
     public partial class MainPageMobile : ContentPage
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnOneCollection { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Product>? ColumnTwoCollection { get; set; }
+
+        public List<Product>? HomePageCollection { get; set; }
+
+        ShoppingCartViewModel shoppingCartViewModel;
 
         public MainPageMobile()
         {
             InitializeComponent();
-            ShoppingCartViewModel shoppingCartViewModel = new ShoppingCartViewModel();
-            rotatorView.ItemsSource = shoppingCartViewModel.RotatorItems;
+            shoppingCartViewModel = new ShoppingCartViewModel();
+            BindingContext = shoppingCartViewModel;
+            ArrangeControlInColumn();
         }
 
         private void ViewAll_Tapped(object sender, TappedEventArgs e)
@@ -18,8 +32,138 @@ namespace ShoppingCart
             TitleLabel.IsVisible = false;
             rotatorView.IsVisible = false;
             ViewAllLabel.IsVisible = false;
-            ProductScrollviewGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            for (int i = 0; i < shoppingCartViewModel.Products.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ColumnOneCollection.Add(shoppingCartViewModel.Products[i]);
+                }
+                else
+                {
+                    ColumnTwoCollection.Add(shoppingCartViewModel.Products[i]);
+                }
+            }
+
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
         }
 
+
+        private void ArrangeControlInColumn()
+        {
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            HomePageCollection = new List<Product>();
+            for (int i=0;i<4;i++)
+            {
+                HomePageCollection.Add(shoppingCartViewModel.Products[i]);
+            }
+
+            for (int i = 0; i < HomePageCollection.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ColumnOneCollection.Add(HomePageCollection[i]);
+                }
+                else
+                {
+                    ColumnTwoCollection.Add(HomePageCollection[i]);
+                }
+            }
+
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
+        }
+
+        private void Menu_Tapped(object sender, TappedEventArgs e)
+        {
+            rotatorView.IsVisible = false;
+            TrendLabel.IsVisible=false;
+            ViewAllLabel.IsVisible=false;
+            if (Application.Current!.RequestedTheme == AppTheme.Dark)
+            {
+                MenuBorder.BackgroundColor = Color.FromArgb("#D0BCFF");
+                MenuLabel.TextColor = Color.FromArgb("#381E72");
+            }
+            else
+            {
+                MenuBorder.BackgroundColor = Color.FromArgb("#6750A4");
+                MenuLabel.TextColor = Color.FromArgb("#FFFFFF");
+            }
+
+            CatagoryScrollView.IsVisible = true;
+            var selectedCategory = "Men";
+            if (selectedCategory != null)
+            {
+
+                if (shoppingCartViewModel != null)
+                {
+                    shoppingCartViewModel.FilteredProducts = new ObservableCollection<Product>();
+                    var filteredProducts = shoppingCartViewModel.Products
+                        .Where(product => product.Category == selectedCategory);
+
+                    foreach (var product in filteredProducts)
+                    {
+                        shoppingCartViewModel.FilteredProducts.Add(product);
+                    }
+                }
+            }
+
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            for (int i = 0; i < shoppingCartViewModel.FilteredProducts?.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ColumnOneCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else
+                {
+                    ColumnTwoCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+            }
+
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
+        }
+
+        private void catagoriesChip_SelectionChanged(object sender, Syncfusion.Maui.Core.Chips.SelectionChangedEventArgs e)
+        {
+            var selectedCategory = e.AddedItem;
+            if (selectedCategory != null)
+            {
+                if (shoppingCartViewModel != null)
+                {
+                    shoppingCartViewModel.FilteredProducts?.Clear();
+
+                    var filteredProducts = shoppingCartViewModel.Products
+                        .Where(product => product.Category == selectedCategory);
+
+                    foreach (var product in filteredProducts)
+                    {
+                        shoppingCartViewModel.FilteredProducts.Add(product);
+                    }
+                }
+            }
+
+            ColumnOneCollection = new List<Product>();
+            ColumnTwoCollection = new List<Product>();
+            for (int i = 0; i < shoppingCartViewModel.FilteredProducts.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    ColumnOneCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+                else
+                {
+                    ColumnTwoCollection.Add(shoppingCartViewModel.FilteredProducts[i]);
+                }
+            }
+
+            BindableLayout.SetItemsSource(this.ColumnOneBorder, ColumnOneCollection);
+            BindableLayout.SetItemsSource(this.ColumnTwoBorder, ColumnTwoCollection);
+        }
     }
 }
