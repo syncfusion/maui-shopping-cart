09b5b062f9e3c1806391775b057fdc4d814efc2a
Karthikasf4617 <karthika.sf4617@gmail.com>
2025-04-30 18:18:12 +0530

    Added products images and updated the viewmodel.

--- ShoppingCart/Views/DesktopView/ProductPageDesktop.xaml.cs
+++ ShoppingCart/Views/DesktopView/ProductPageDesktop.xaml.cs
@@ -3,12 +3,9 @@ namespace ShoppingCart;
 
 public partial class ProductPageDesktop : ContentView
 {
-	List<string> SizeCatagory = new List<string>() { "S","M","L","XL","2XL","3XL","4XL"};
 	public ProductPageDesktop()
 	{
 		InitializeComponent();
-        BindableLayout.SetItemsSource(SizeLayout, SizeCatagory);
-
     }
 
     private void BackArrow_Tapped(object sender, TappedEventArgs e)
